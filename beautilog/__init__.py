95506e5ed5bfaca9e8bace865512efa55efea9f2
Parth Patil <parthvin@gmail.com>
2025-08-05 12:03:21 -0400

    Added config parse and switched to ini files

--- beautilog/__init__.py
+++ beautilog/__init__.py
@@ -1,10 +1,12 @@
-# fancy_log/__init__.py
+from logging import Logger
+
+# beautilog/__init__.py
 from .beauti_logger import get_logger
 from .color_console_handler import ColoredConsoleHandler
 from .constants import TERMINAL_COLORS
 
 # Initialize logger at import time
-logger = get_logger()
+logger: Logger = get_logger()
 
 # Optional: expose utility functions if needed later
 __all__ = [
--- beautilog/beauti_logger.py
+++ beautilog/beauti_logger.py
@@ -9,39 +9,18 @@ from logging.handlers import RotatingFileHandler
 from tqdm import tqdm
 
 from .color_console_handler import ColoredConsoleHandler
+from .config import Config
 from .constants import TERMINAL_COLORS
 
-CONFIG_FILE = "beauti-log.json"
 
-ABSOLUTE_PATH = os.path.abspath(os.path.dirname(__file__))
-
-CONFIG_FILE_ABS_PATH = os.path.join(ABSOLUTE_PATH, CONFIG_FILE)
-with open(CONFIG_FILE_ABS_PATH, 'r') as f:
-    DEFAULT_CONFIG = json.load(f)
-
-
-def load_config():
-    paths = [
-        os.path.join(os.getcwd(), CONFIG_FILE),
-        os.path.join(os.path.dirname(__file__), "..", CONFIG_FILE)
-    ]
-    for path in paths:
-        if os.path.exists(path):
-            with open(path, 'r') as f:
-                CONFIG = json.load(f)
-                DEFAULT_CONFIG.update(CONFIG)
-                return DEFAULT_CONFIG
-
-    return DEFAULT_CONFIG
-
-def get_logger():
-    config = load_config()
+def get_logger() -> logging.Logger:
+    config = Config()
     logger = logging.getLogger()
 
-    if config.get("suppress_other_loggers", False):
+    if config.logger.get("suppress_existing_loggers", False):
         logger.handlers.clear()
 
-    for mute_loggger_name in config.get("disable_loggers",[]):
+    for mute_loggger_name in config.logger.get("disabled_loggers", []):
         mute_logger = logging.getLogger(mute_loggger_name)
         mute_logger.setLevel(logging.CRITICAL)
         mute_logger.handlers.clear()
@@ -51,17 +30,16 @@ def get_logger():
         datefmt='%m/%d/%y %I:%M:%S %p'
     )
 
-    if config.get("save_to_file", True):
-        file_logger_config = config.get("file_logger", {})
+    if config.logger.get("save_to_file", True):
         file_handler = RotatingFileHandler(
-            file_logger_config.get("log_file_path", "fancy-run.log"),
+            config.file_logger.get("log_file_path", "fancy-run.log"),
             mode='a',
-            maxBytes=file_logger_config.get("max_bytes", 10485760),  # Default 100 MB
-            backupCount=file_logger_config.get("backup_count", 5),
+            maxBytes=config.file_logger.get("max_bytes", 10485760),  # Default 100 MB
+            backupCount=config.file_logger.get("backup_count", 5),
             encoding='utf-8'
         )
         file_handler.setFormatter(formatter)
-        file_handler.setLevel(getattr(logging, file_logger_config.get("log_level", "DEBUG").upper(), logging.DEBUG))
+        file_handler.setLevel(getattr(logging, config.file_logger.get("log_level", "DEBUG").upper(), logging.DEBUG))
         logger.addHandler(file_handler)
 
     def write(self, x):
@@ -69,14 +47,14 @@ def get_logger():
             tqdm.write(x, file=self.file, end='')
     out_stream = type("TqdmStream", (), {'file': sys.stdout, 'write': write})()
 
-    console_handler = ColoredConsoleHandler(out_stream, config.get("level_colors", {}))
+    console_handler = ColoredConsoleHandler(out_stream, config.sections.get("level_colors", {}))
     console_handler.setFormatter(formatter)
-    console_handler.setLevel(getattr(logging, config.get("log_level", "INFO").upper(), logging.INFO))
+    console_handler.setLevel(getattr(logging, config.sections.get("log_level", "INFO").upper(), logging.INFO))
 
     logger.addHandler(console_handler)
-    logger.setLevel(getattr(logging, config.get("log_level", "INFO").upper(), logging.INFO))
+    logger.setLevel(getattr(logging, config.logger.get("log_level", "INFO").upper(), logging.INFO))
 
-    for level_name, level_value in config.get("custom_levels", {}).items():
+    for level_name, level_value in config.sections.get("custom_levels", {}).items():
         logging.addLevelName(level_value, level_name.upper())
         setattr(logging, level_name.upper(), level_value)
         setattr(logger, level_name.upper(), level_value)
